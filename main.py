2dc2969337feb9d7219ec707615f88574bb3d05b
Sargin Stas <stassargin@gmail.com>
2020-07-10 15:35:09 +0300

    Внес правки и добавил подскази по регулярным выражениям.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+
+
 # import csv
 # import re
 
@@ -7,7 +9,7 @@ from bs4 import BeautifulSoup
 # Функция получения html.
 def get_html(url):
     request = requests.get(url)
-    if request.ok:                  # Сервер вернул код 200.
+    if request.ok:  # Сервер вернул код 200.
         return request.text
     else:
         print(request.status_code)  # Выводим код ошибки.
@@ -26,8 +28,8 @@ def get_html(url):
 # # Функция №2 записи в csv.
 # def write_csv2(data):
 #     with open('file.csv', 'a') as file:
-#         order = ['name', 'link']
-#         writer = csv.DictWriter(file, fieldnames=order)
+#         fieldnames = ['name', 'link']
+#         writer = csv.DictWriter(file, fieldnames=fieldnames)
 #
 #         writer.writerow(data)
 
@@ -36,16 +38,41 @@ def get_html(url):
 #     result = string.split(' ')[1]
 #     return result
 
+"""
+Поиск элементов в BeautifulSoup.
+.find() - Найти первый элемент с указанными атрибутами.
+.find_all() - Найти все элементы с указанными атрибутами. Получаем список объектов BeautifulSoup.
+.parent - Найти родителя элемента.
+.find_parent() - Найти родителя элемента с указанными атрибутами.
+.find_next_sibling() - Найти следующего брата с указанными атрибутами.
+.find_previous_sibling() - Найти предыдущего брата с указанными атрибутами.
+"""
+
+"""
+Регулярные выражения.
+pattern = r'\d' - Найти все цифры в строке.
+items = re.findall(pattern, place) - Возвращает список. В атрибутах указываем что и где искать.
+
+pattern = re.compile('\d') - Найти все строки с цифрами.
+
+^ - Начало строки.
+$ - Конец строки.
+. - Любой символ.
++ - Неограниченное число вхождений.
+'\d' - Цифра.
+'\w' - Буквы, цифры, _ .
+"""
+
 
 # Функция получения данных.
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')  # Вторым атрибутом указываем нужный парсер.
-    items = soup.find_all('div', class_="model")
+    items = soup.find_all('div', class_="model")  # Атрибуты можно записать так: ('div', {'class': model'}).
     for item in items:
         try:
             name = item.find_all('a')[0].text.strip()
         except:
-            name = ''                   # Если значения нет, то записываем пустую строку.
+            name = ''  # Если значения нет, то записываем пустую строку.
 
         try:
             link = 'https://www.yandex.ru' + item.find_all('a')[0].get('href')
